eeba8a4c7f9dc3d8a2a5243e288c682142a33f80
fred-corp <Frederic.druppel@icloud.com>
2024-07-17 14:37:10 +0200

    added distance sensor code

---
+++ Capteur_distance/Capteur_distance.ino
@@ -0,0 +1,24 @@
+#define TRIG_PIN 7
+#define ECHO_PIN 8
+
+
+void setup() {
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(10);
+
+  long temps = pulseIn(ECHO_PIN, HIGH);
+
+  // calculer la distance en centimètres
+  float distance = (temps * 340.0 / 10000.0) / 2.0;
+  Serial.println(distance);
+  delay(50);
+}
