115147fd872ce2d8ddbd1e5f16c1ab712511576b
fred-corp <Frederic.druppel@icloud.com>
2024-07-18 09:29:08 +0200

    added pins

--- Code_Robot_Complet/Code_Robot_Complet.ino
+++ Code_Robot_Complet/Code_Robot_Complet.ino
@@ -1,7 +1,17 @@
 #include <Servo.h>
 
 // Broches
-
+// H-Bridge
+#define IN1        3
+#define IN2        5
+#define IN3        6
+#define IN4        9
+// Servomoteur
+#define SERVO_PIN 10
+Servo monServo;
+// Capteur de distance
+#define TRIG_PIN   7
+#define ECHO_PIN   8
 
 // Valeurs clés
 #define ANGLE_FACE    90
@@ -13,8 +23,7 @@
 #define OBSTACLE 1
 #define MANUEL   2
 
-
-int etat = 0;
+int etat = NORMAL;
 
 
 void setup() {
