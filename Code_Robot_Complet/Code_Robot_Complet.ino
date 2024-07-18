4a34c85d9ef29bf9654dfc038936777d1495b3a7
fred-corp <Frederic.druppel@icloud.com>
2024-07-18 09:21:31 +0200

    added complete robot code

---
+++ Code_Robot_Complet/Code_Robot_Complet.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+// Broches
+
+
+// Valeurs clés
+#define ANGLE_FACE    90
+#define ANGLE_GAUCHE 180
+#define ANGLE_DROIT    0
+
+// mots-clés
+#define NORMAL   0
+#define OBSTACLE 1
+#define MANUEL   2
+
+
+int etat = 0;
+
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
